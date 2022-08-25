ac6047105afe844c29b9951a932c92e616065862
hectordan22 <salsegovi21@gmail.com>
2022-08-25 13:18:26 -0400

    Se agrega el proyecto

---
+++ activos.js
@@ -0,0 +1,60 @@
+document.getElementById("servi").addEventListener('click',(event)=>{
+    event.preventDefault()
+    document.getElementsByClassName("home2")[0].scrollIntoView(true)
+    window.scrollBy(0,-200)
+
+    
+   
+})
+
+document.getElementById("conta").addEventListener('click',(event)=>{
+    event.preventDefault()
+    document.getElementsByClassName("home4")[0].scrollIntoView(true)
+    window.scrollBy(0,-200)
+
+    
+   
+})
+
+
+document.getElementById("ubic").addEventListener('click',(event)=>{
+    event.preventDefault()
+    document.getElementsByClassName("home3")[0].scrollIntoView(true)
+    window.scrollBy(0,-200)
+
+    
+   
+})
+
+
+
+// lo mismo pero para el caso que el usuario de click a los li
+document.getElementById("servi1").addEventListener('click',(event)=>{
+    event.preventDefault()
+    document.getElementsByClassName("home2")[0].scrollIntoView(true)
+    window.scrollBy(0,-200)
+
+    
+   
+})
+
+document.getElementById("conta1").addEventListener('click',(event)=>{
+    event.preventDefault()
+    document.getElementsByClassName("home4")[0].scrollIntoView(true)
+    window.scrollBy(0,-200)
+
+    
+   
+})
+
+
+document.getElementById("ubic1").addEventListener('click',(event)=>{
+    event.preventDefault()
+    document.getElementsByClassName("home3")[0].scrollIntoView(true)
+    window.scrollBy(0,-200)
+
+    
+   
+})
+
+
